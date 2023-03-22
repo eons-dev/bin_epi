14de55bee77eeab27bb7eb7f1f1c8055b5e0a6d3
John Ellis Manning-Villar <ellismv11@gmail.com>
2023-03-22 16:52:37 -0700

    Undid previous commit

--- src/EMI.py
+++ src/EMI.py
@@ -27,7 +27,7 @@ class EMI(EBBS):
 		this.catalog = orm.sessionmaker(bind=this.sqlEngine)() # sqlalchemy: sessionmaker()->Session()->session.
 		this.SetupHome()
 
-		eons.Executor.__init__(name="Eons Modular Interface", descriptionStr="A universal state manager.")
+		super().__init__(name="Eons Modular Interface", descriptionStr="A universal state manager.")
 
 		# Windows paths must be set in the config.json.
 		this.paths = [
